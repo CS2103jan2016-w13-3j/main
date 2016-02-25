91aa65d430746f287b35d5edc9fed002fde5dba0
ishpal26 <ishpalsinghgill@live.co.uk>
2016-02-25 15:23:36 +0800

    test

--- src/Logic.java
+++ src/Logic.java
@@ -1,9 +1,15 @@
 
 public class Logic {
-
+	
+	private static Parser parser;
+	private static Storage storage;
+	private static Arraylist<Task> list;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	Parser parser = new Parser();
+	Storage storage = new Storage();
+	Arraylist<Task> list = new Arraylist();
 	}
 
 }
