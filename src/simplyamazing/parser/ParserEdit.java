9e9902c869bdf0ae708b2d8d85979a397b807112
A0112659 <A0112659@u.nus.edu>
2016-03-11 19:18:22 +0800

    modify the ParserEdit

--- src/simplyamazing/parser/ParserEdit.java
+++ src/simplyamazing/parser/ParserEdit.java
@@ -1,22 +1,7 @@
 package simplyamazing.parser;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class ParserEdit {
-	private static final String CHARACTER_SPACE = "\\s";
-	private static final String TIME_FORMAT = "H:mm dd MMM yyyy";
-	private static String firstWord = "";
-	private static String removeFirstWord = "";
-	private static final String EMPTY_STRING = "";
-	private static final String DESCRIPTON = "description";
-	private static final String STARTTIME = "startTime";
-	private static final String ENDTIME = "endTime";
-	private static final String PRIORITY = "priority";
-	private static final String DONE = "done";
 	private static final String MESSAGE_INVALID_FORMAT = "The command of input's field is invalid";
-	private static boolean isValid;
 
 	public Handler parseEditCommand(Handler handler, String taskIndex, String taskInfoWithoutIndex) throws Exception {
 		if (isInteger(taskIndex)) {
@@ -27,8 +12,7 @@ public class ParserEdit {
 			return handler;
 		}
 		String[] fieldValuePairs = taskInfoWithoutIndex.split(",");
-		isValid = isEditingValid(taskInfoWithoutIndex);
-		if (isValid) {
+		
 			for (int i = 0; i < fieldValuePairs.length; i++) {
 				String field = Parser.getFirstWord(fieldValuePairs[i]);
 				String value = Parser.removeFirstWord(fieldValuePairs[i]);
@@ -51,10 +35,6 @@ public class ParserEdit {
 					handler.setFeedBack(MESSAGE_INVALID_FORMAT);
 				}
 			}
-		} else {
-			handler.setHasError(true);
-			handler.setFeedBack(MESSAGE_INVALID_FORMAT);
-		}
 		return handler;
 	}
 
@@ -68,67 +48,5 @@ public class ParserEdit {
 		}
 		// only got here if we didn't return false
 		return true;
-	}
-
-	public boolean isEditingValid(String taskInfo) throws Exception {
-		firstWord = taskInfo.trim().split(CHARACTER_SPACE)[0];
-		removeFirstWord = taskInfo.replaceFirst(firstWord, EMPTY_STRING).trim();
-
-		if (firstWord.equals(EMPTY_STRING)) {
-			return false;
-		} else if (firstWord.equals(DONE)) {
-			if (removeFirstWord.equals("true") || removeFirstWord.equals("false")) {
-				return true;
-			} else {
-				return false;
-			}
-		} else if (firstWord.equals(DESCRIPTON)) {
-			return true;
-		} else if (firstWord.equals(STARTTIME)) {
-			if (removeFirstWord.equals("")) {
-				return false;
-			} else {
-				try {
-					SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT);
-					sdf.setLenient(false);
-					Date startDate = sdf.parse(removeFirstWord);
-					Date todayDate = sdf.parse(sdf.format(new Date()));
-
-					if (startDate.before(todayDate)) {
-						return false;
-					} else {
-						return true;
-					}
-				} catch (ParseException e) {
-					return false;
-				}
-			}
-		} else if (firstWord.equals(ENDTIME)) {
-			if (removeFirstWord.equals("")) {
-				return false;
-			} else {
-				try {
-					SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT);
-					sdf.setLenient(false);
-					Date endDate = sdf.parse(removeFirstWord);
-					Date todayDate = sdf.parse(sdf.format(new Date()));
-
-					if (endDate.before(todayDate)) {
-						return false;
-					} else {
-						return true;
-					}
-				} catch (ParseException e) {
-					return false;
-				}
-			}
-		} else if (firstWord.equals(PRIORITY)) {
-			if (removeFirstWord.equals("low") || removeFirstWord.equals("high") || removeFirstWord.equals("medium")) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-		return false;
 	}
 }
