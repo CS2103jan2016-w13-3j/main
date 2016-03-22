5dd0a0b06191b1e9359ff67ff9b396086e84208e
ishpal26 <ishpalsinghgill@live.co.uk>
2016-03-22 22:57:37 +0800

    added setLocationTest

--- src/simplyamazing/logic/LogicTest.java
+++ src/simplyamazing/logic/LogicTest.java
@@ -9,19 +9,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class LogicTest {
-	private static ArrayList<Task> list;
-	private static Logic logicObj;
+	private static ArrayList<Task> list = new ArrayList<Task>();;
+	private static Logic logicObj= new Logic();;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws ClassNotFoundException, IOException{
-		logicObj = new Logic();
-		list = new ArrayList<Task>();
-	}
+	
+	private static final String LOCATION_COMMAND_FAIL = "location just a placeholder";
+	private static final String LOCATION_COMMAND_PASS = "location C:\"Users\"Ishpal\"Desktop\"Task Data";
+	private static final String LOCATION_FEEDBACK_FAIL = "Provided storage location is not a valid directory.";
+	private static final String LOCATION_FEEDBACK_PASS = "storage location of task data has been successfully set";
+	
+	
 	
 	
 	@Test
+	public void testSetLocation() throws Exception{
+		assertEquals(new Exception(LOCATION_FEEDBACK_FAIL),logicObj.executeCommand(LOCATION_COMMAND_FAIL));
+		assertEquals(new Exception(LOCATION_FEEDBACK_PASS), logicObj.executeCommand(LOCATION_COMMAND_PASS));
+	}
+	
+	@Test
 	public void testValidCommandTypes() throws Exception {
-		//assertEquals(new Exception("the add command is not correct"),logicObj.executeCommand("add "));
+		assertEquals(new Exception("the add command is not correct"),logicObj.executeCommand("add "));
 		assertEquals(new Exception("Error: Invalid command entered. Please enter \"help\" to view command format"), logicObj.executeCommand("hi"));
 		assertEquals(new Exception("there is no previous command to undo"), logicObj.executeCommand("undo"));
 		assertEquals(new Exception("the index of deleting is invalid"), logicObj.executeCommand("delete"));
