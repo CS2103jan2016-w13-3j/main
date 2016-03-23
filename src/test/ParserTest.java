9fc503b5369722bd49e0aee6337fc6a6f214e679
A0112659 <A0112659@u.nus.edu>
2016-03-23 10:20:30 +0800

    add some bondary value analysis

--- src/test/ParserTest.java
+++ src/test/ParserTest.java
@@ -38,6 +38,8 @@ public class ParserTest {
 	private static final String DELETE_COMMAND_VALID_INDEX = "delete 1";
 	private static final String DELETE_COMMAND_VALID_INDEX_FEEDBACK = "";
 	private static final String DELETE_COMMAND_INVALID_DUMMY_STRING = "delete go home";
+	private static final String DELETE_COMMAND_INVALID_WITH_NEGATIVE_INDEX = "delete go home";
+	private static final String DELETE_COMMAND_INVALID_WITH_ZERO_INDEX = "delete go home";
 	private static final String DELETE_COMMAND_INVALID_FEEDBACK = "the index of deleting is invalid";
     
 	private static final String LOCATION_COMMAND_INVALID = "location ";
@@ -80,7 +82,8 @@ public class ParserTest {
 	public void testDeleteCommand() throws Exception{
 		assertEquals(DELETE_COMMAND_VALID_INDEX_FEEDBACK, parser.getHandler(DELETE_COMMAND_VALID_INDEX).getFeedBack());
 		assertEquals(DELETE_COMMAND_INVALID_FEEDBACK, parser.getHandler(DELETE_COMMAND_INVALID_DUMMY_STRING).getFeedBack());
-		assertEquals(DELETE_INVALID_INDEX_FEEDBACK, logicObj.executeCommand(DELETE_INVALID_INDEX_NEGATIVE));
-		assertEquals(DELETE_INVALID_INDEX_FEEDBACK, logicObj.executeCommand(DELETE_INVALID_INDEX_ZERO));
+		//these two test cases uses the Boundary value analysis
+		assertEquals(DELETE_COMMAND_INVALID_FEEDBACK, parser.getHandler(DELETE_COMMAND_INVALID_WITH_NEGATIVE_INDEX).getFeedBack());
+		assertEquals(DELETE_COMMAND_INVALID_FEEDBACK, parser.getHandler(DELETE_COMMAND_INVALID_WITH_ZERO_INDEX).getFeedBack());
 	}
 }
