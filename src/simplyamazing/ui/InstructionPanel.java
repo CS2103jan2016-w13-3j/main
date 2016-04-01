8c81d2740aa8e611ffc12456852863f6377a54cb
leshweyeewin <www.pancherry.com@gmail.com>
2016-04-01 18:59:10 +0800

    updated UI to display task table every time

---
+++ src/simplyamazing/ui/InstructionPanel.java
@@ -0,0 +1,32 @@
+package simplyamazing.ui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+
+public class InstructionPanel {
+	private static final String STRING_NULL = "";
+	private JTextPane instructionPanel;
+	
+	public InstructionPanel() {
+		instructionPanel = new JTextPane();
+		instructionPanel.setForeground(Color.BLACK);
+		instructionPanel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		instructionPanel.setEditable(false);
+		instructionPanel.setBounds(10, 57, 664, 311);
+	}
+	
+	public JTextPane getInstrctionPanel() {
+		return instructionPanel;
+	}
+	
+	public void setInstruction(String instruction) {
+		instructionPanel.setText(instruction);
+	}
+	
+	public void clear() {
+		instructionPanel.setText(STRING_NULL);
+	}
+}
