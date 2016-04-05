c38be421d9ee8a38c0b60837301d9a6ea064a7ce
A0112659 <A0112659@u.nus.edu>
2016-04-05 12:45:16 +0800

    Modify the ParserAdd to satisfy the valid checking for standard format

--- src/simplyamazing/parser/ParserAdd.java
+++ src/simplyamazing/parser/ParserAdd.java
@@ -26,7 +26,7 @@ public class ParserAdd {
 	private static Date endingDate = null;
 	private static int startTimeIndex;
 	private static int endTimeIndex;
-
+	private static int year;
 	private boolean checkValue = false, isEvent = false, isDeadline = false, isFloatingTask = false;
 
 	private static Logger logger = Logger.getLogger("ParserAdd");
@@ -56,7 +56,7 @@ public class ParserAdd {
 	public boolean isAddingValid(Handler handler,String taskInfo) throws Exception {
 		com.joestelmach.natty.Parser dateParser = new com.joestelmach.natty.Parser();
 
-		if (taskInfo.contains(KEYWORD_SCHEDULE_FROM) && taskInfo.contains(KEYWORD_SCHEDULE_TO) && taskInfo.contains(STRING_TIME_FORMATTER)) {
+		if (taskInfo.contains(KEYWORD_SCHEDULE_FROM) && taskInfo.contains(KEYWORD_SCHEDULE_TO)) {
 			System.out.println("Found "+KEYWORD_SCHEDULE_FROM+", "+KEYWORD_SCHEDULE_TO+","+STRING_TIME_FORMATTER);
 			startTimeIndex = taskInfo.lastIndexOf(KEYWORD_SCHEDULE_FROM);
 			endTimeIndex = taskInfo.lastIndexOf(KEYWORD_SCHEDULE_TO);
@@ -71,13 +71,72 @@ public class ParserAdd {
 					handler.setFeedBack(ERROR_MESSAGE_FIELDS_NOT_CORRECT);
 					return false;
 				} else if (!startTime.equals(EMPTY_STRING) && !endTime.equals(EMPTY_STRING)) {
-					
 					boolean isStartFormatCorrect = followStandardFormat(startTime);
 					boolean isEndFormatCorrect = followStandardFormat(endTime);
-					if(isStartFormatCorrect == false || isEndFormatCorrect == false){
-						// use natty instead of standard format
+
+					SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT,Locale.ENGLISH);
+					sdf.setLenient(false);
+					if(isStartFormatCorrect == false && isEndFormatCorrect == false){
+						List<DateGroup> dateGroup1 = dateParser.parse(startTime);
+						List<DateGroup> dateGroup2 = dateParser.parse(endTime);
+
+						if(dateGroup1.isEmpty()||dateGroup2.isEmpty()){
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}						
+						List<Date> date1 = dateGroup1.get(0).getDates();
+						startingDate = date1.get(0);
+						List<Date> date2 = dateGroup2.get(0).getDates();
+						endingDate = date2.get(0);
+					}else if (isStartFormatCorrect == true && isEndFormatCorrect == true){
+						try{
+							startingDate = (Date)sdf.parse(startTime);
+							endingDate = (Date)sdf.parse(endTime);
+						}catch (ParseException e) {
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}
+					}else if (isStartFormatCorrect == true && isEndFormatCorrect == false){
+						try{
+							startingDate = (Date)sdf.parse(startTime);
+						}catch (ParseException e) {
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}
+						List<DateGroup> dateGroup2 = dateParser.parse(endTime);
+
+						if(dateGroup2.isEmpty()){
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}						
+						List<Date> date2 = dateGroup2.get(0).getDates();
+						endingDate = date2.get(0);
+					}else if(isStartFormatCorrect == false && isEndFormatCorrect == true){
+						try{
+							endingDate = (Date)sdf.parse(endTime);
+						}catch (ParseException e) {
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}
+						List<DateGroup> dateGroup2 = dateParser.parse(startTime);
+
+						if(dateGroup2.isEmpty()){
+							handler.setHasError(true);
+							handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+							return false;
+						}						
+						List<Date> date2 = dateGroup2.get(0).getDates();
+						startingDate = date2.get(0);
 					}
-					
+
+
+
+					/*
 					List<DateGroup> dateGroup1 = dateParser.parse(startTime);
 					List<DateGroup> dateGroup2 = dateParser.parse(endTime);
 
@@ -91,26 +150,27 @@ public class ParserAdd {
 						/*Date startingDate = null;
 					startingDate = (Date)sdf.parse(startTime);
 					Date endingDate = null;
-					endingDate = (Date)sdf.parse(endTime); */
+					endingDate = (Date)sdf.parse(endTime); //
 						List<Date> date1 = dateGroup1.get(0).getDates();
 						startingDate = date1.get(0);
 						List<Date> date2 = dateGroup2.get(0).getDates();
 						endingDate = date2.get(0);
-						Date todayDate = null;
-						todayDate = (Date)sdf.parse(sdf.format(new Date()));
+					 */
+					Date todayDate = null;
+					todayDate = (Date)sdf.parse(sdf.format(new Date()));
 
-						if (startingDate.after(endingDate)||startingDate.compareTo(endingDate) == 0) {
-							handler.setHasError(true);
-							handler.setFeedBack(ERROR_MESSAGE_START_AFTER_END);
-							return false;
-						} else if (!startingDate.after(todayDate) || !endingDate.after(todayDate)) {
-							handler.setHasError(true);
-							handler.setFeedBack(ERROR_MESSAGE_DATE_BEFORE_CURRENT);
-							return false;
-						} else {
-							isEvent = true;
-							return true;
-						}
+					if (startingDate.after(endingDate)||startingDate.compareTo(endingDate) == 0) {
+						handler.setHasError(true);
+						handler.setFeedBack(ERROR_MESSAGE_START_AFTER_END);
+						return false;
+					} else if (!startingDate.after(todayDate) || !endingDate.after(todayDate)) {
+						handler.setHasError(true);
+						handler.setFeedBack(ERROR_MESSAGE_DATE_BEFORE_CURRENT);
+						return false;
+					} else {
+						isEvent = true;
+						return true;
+					}
 				}
 			} else if(taskInfo.contains(KEYWORD_DEADLINE)){
 				endTimeIndex = taskInfo.lastIndexOf(KEYWORD_DEADLINE);
@@ -125,7 +185,7 @@ public class ParserAdd {
 				}
 			}
 		}
-		if (taskInfo.contains(KEYWORD_DEADLINE) && taskInfo.contains(STRING_TIME_FORMATTER)) {
+		if (taskInfo.contains(KEYWORD_DEADLINE)) {
 			System.out.println("Found "+KEYWORD_DEADLINE+", "+STRING_TIME_FORMATTER);
 			System.out.println("This is a deadline task");
 			endTimeIndex = taskInfo.lastIndexOf(KEYWORD_DEADLINE);
@@ -137,32 +197,53 @@ public class ParserAdd {
 				handler.setFeedBack(ERROR_MESSAGE_FIELDS_NOT_CORRECT);
 				return false;
 			} else if (!endTime.equals(EMPTY_STRING)) {
-				List<DateGroup> dateGroup3 = dateParser.parse(endTime);
+				boolean isEndFormatCorrect = followStandardFormat(endTime);
 
-				if(dateGroup3.isEmpty()){
-					handler.setHasError(true);
-					handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
-					return false;
+				SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT,Locale.ENGLISH);
+				sdf.setLenient(false);
+
+				if(isEndFormatCorrect == false){
+                    System.out.println("Endtime use Natty");
+					List<DateGroup> dateGroup3 = dateParser.parse(endTime);
+
+					if(dateGroup3.isEmpty()){
+						handler.setHasError(true);
+						handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+						return false;
+					}
+					List<Date> date3 = dateGroup3.get(0).getDates();
+					endingDate = date3.get(0);
+				}else if (isEndFormatCorrect == true){
+					System.out.println("Endtime use our format");
+					try{
+						endingDate = (Date)sdf.parse(endTime);
+					}catch (ParseException e) {
+						handler.setHasError(true);
+						handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+						return false;
+					}
 				}
+
+				Date todayDate = null;
+				todayDate = (Date)sdf.parse(sdf.format(new Date()));
+
+				/*
 				try {
 					SimpleDateFormat sdf = new SimpleDateFormat(TIME_FORMAT, Locale.ENGLISH);
 					sdf.setLenient(true);
-					/*Date endingDate = null;
-					endingDate = (Date)sdf.parse(endTime);*/
+					//Date endingDate = null;
+					endingDate = (Date)sdf.parse(endTime);//
 					Date todayDate = null;
 					todayDate = (Date)sdf.parse(sdf.format(new Date()));
 					List<Date> date3 = dateGroup3.get(0).getDates();
 					endingDate = date3.get(0);
-					if (!endingDate.after(todayDate)) {
-						handler.setHasError(true);
-						handler.setFeedBack(ERROR_MESSAGE_DATE_BEFORE_CURRENT);
-						return false;
-					} 
-				} catch (ParseException e) {
+				 */
+
+				if (!endingDate.after(todayDate)) {
 					handler.setHasError(true);
-					handler.setFeedBack(ERROR_MESSAGE_TIME_FORMAT_INVALID);
+					handler.setFeedBack(ERROR_MESSAGE_DATE_BEFORE_CURRENT);
 					return false;
-				}
+				} 			
 			}
 			isDeadline = true;
 			return true;
@@ -182,12 +263,12 @@ public class ParserAdd {
 		isFloatingTask = true;
 		return true;
 	}
-	
-	
-	
+
+
+
 	public boolean followStandardFormat(String dateTimeString){
 		String[] dateTimeArr = dateTimeString.trim().split(" ");
-		
+
 		if (dateTimeArr.length != 4) {
 			return false;
 		} else {
@@ -206,7 +287,7 @@ public class ParserAdd {
 				if ((date > 31) || (date < 0)) {	//date given not valid
 					return false;
 				} else {
-					
+
 					String givenMonth = dateTimeArr[2].toLowerCase();
 					if ( !(givenMonth.contains("jan") || givenMonth.contains("feb") || givenMonth.contains("mar") ||givenMonth.contains("apr")
 							|| givenMonth.contains("may") || givenMonth.contains("jun") || givenMonth.contains("jul") || givenMonth.contains("aug")
@@ -214,7 +295,6 @@ public class ParserAdd {
 						// month given follows the required format
 						return false;
 					} else {
-						int year;
 						try {
 							year = Integer.parseInt(dateTimeArr[3], 10);
 						} catch (NumberFormatException e) {
