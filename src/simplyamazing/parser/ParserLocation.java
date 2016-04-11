f73dfac52d740010fce138899d9c0b516fd55e16
A0112659 <A0112659@u.nus.edu>
2016-04-11 13:55:49 +0800

    finish the logging for the ParserLocation

--- src/simplyamazing/parser/ParserLocation.java
+++ src/simplyamazing/parser/ParserLocation.java
@@ -1,6 +1,7 @@
 //@@author A0112659A
 package simplyamazing.parser;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ParserLocation {
@@ -8,8 +9,9 @@ public class ParserLocation {
 	private static final String INVALID_LOCATION = "Error: Location provided is invalid";
 
 	public Handler parseLocationCmd(Handler handler, String taskInfo, Logger logger) throws Exception {
-		
+		logger.log(Level.INFO,"start to parse the location command");
 		if (taskInfo.equals(STRING_EMPTY)) {
+			logger.log(Level.WARNING,"the location address is empty");
 			handler.setHasError(true);
 			handler.setFeedBack(INVALID_LOCATION);
 		} else {
