fcb137eeb7cb9c1f4f851c0f1e90145fa976331e
A0112659 <A0112659@u.nus.edu>
2016-04-11 13:33:26 +0800

    Finish the logging in the ParserDelete

--- src/simplyamazing/parser/ParserDelete.java
+++ src/simplyamazing/parser/ParserDelete.java
@@ -1,6 +1,7 @@
 //@@author A0112659A
 package simplyamazing.parser;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ParserDelete {
@@ -12,7 +13,7 @@ public class ParserDelete {
 		String[] indexes = taskInfo.split(SPACE);
 		size = indexes.length;
 		for (int i = 0; i < size;i++){
-			if (isInteger(indexes[i])){
+			if (isInteger(indexes[i],logger)){
 				handler.setIndex(indexes[i]);
 			}else{
 				handler.setHasError(true);
@@ -22,10 +23,12 @@ public class ParserDelete {
 		}
 		return handler;
 	}
-	public static boolean isInteger(String taskInfo) {
+	public static boolean isInteger(String taskInfo, Logger logger) {
+		logger.log(Level.INFO,"start to analyze the index");
 		try { 
 			Integer.parseInt(taskInfo); 
-		} catch(NumberFormatException e) { 
+		} catch(NumberFormatException e) {
+			logger.log(Level.WARNING, "the index is not an Integer");
 			return false; 
 		}
 		// only got here if we didn't return false
